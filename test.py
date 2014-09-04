f48b386aedc217e31da751f543ce855020c8b7b7
olorin <olorin@yandex-team.ru>
2014-09-04 20:15:43 +0400

    fix nonlambda realization of qsort

--- test.py
+++ test.py
@@ -25,15 +25,17 @@ def sort(mylist, low, high):
                 i += 1
                 j -= 1
         if i < high: sort(mylist, i, high)
-        if low < j: sort(mylist, low+1, j)
+        if low < j: sort(mylist, low, j)
     return mylist
 
 def qsort(L):
     if L: return qsort(filter(lambda x: x < L[0], L)) + L[0:1] + qsort(filter(lambda x: x > L[0], L))
     return []
 
-myarray = [20,267,7,10,5]
+myarray = [20,234,32434,22,1,98,267,7,10,1231]
 print(myarray)
+print("sorted array by my qsort",sort(myarray,0,len(myarray)-1))
+print("sorted array by lambda sort", qsort(myarray))
 print(gettime(sort,myarray,0,len(myarray)-1))
 print(gettime(qsort,myarray))
 
